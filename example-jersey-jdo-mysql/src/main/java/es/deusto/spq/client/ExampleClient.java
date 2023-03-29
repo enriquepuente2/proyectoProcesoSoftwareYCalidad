22de1404b67f1f2a33dfffd5d5eaa621f9035734
ismaelalcaide <91288325+ismaelalcaide@users.noreply.github.com>
2023-03-29 10:32:21 +0200

    w

--- example-jersey-jdo-mysql/src/main/java/es/deusto/spq/client/ExampleClient.java
+++ example-jersey-jdo-mysql/src/main/java/es/deusto/spq/client/ExampleClient.java
@@ -22,8 +22,7 @@ public class ExampleClient {
 
 	private static final String USER = "dipina";
 	private static final String PASSWORD = "dipina";
-	//dfgjksdahg
-	///erjvnekirvneirvbneiruvbi3bvi3ugbi3ug
+
 	private Client client;
 	private WebTarget webTarget;
 
